da2a0a4c74dd4be205b667e2cab82aa9fe49255c
e <2540098059@qq.com>
2020-11-26 22:08:45 +0800

    给定一个整形数组arr，已知其中所有的值都是非负的，将这个数组看作一个容器，请返回容器能装多少水。

---
+++ 容器盛水问题/src/Solution.java
@@ -0,0 +1,32 @@
+import java.util.*;
+public class Solution {
+    public long maxWater (int[] arr) {
+
+        if(arr.length == 0 || arr.length <= 2)
+            return 0;
+        int left = 0, right = arr.length-1;
+        long res = 0;
+        //取低的为边界
+        int min = Math.min(arr[left],arr[right]);
+
+        while(left < right){
+            if(arr[left] < arr[right]){
+                left++;
+                //如果当前水位小于边界，则可以装水
+                if(arr[left] < min){
+                    res += min-arr[left];
+                }else{
+                    min = Math.min(arr[left],arr[right]);
+                }
+            }else{
+                right--;
+                if(arr[right] < min){
+                    res += min-arr[right];
+                }else{
+                    min = Math.min(arr[right],arr[left]);
+                }
+            }
+        }
+        return res;
+    }
+}
