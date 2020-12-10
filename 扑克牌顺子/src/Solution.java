e3aef08621b8500fa6996282e8676ede31f86e49
e <2540098059@qq.com>
2020-12-10 21:42:51 +0800

    大\小王可以看成任何数字,并且A看作1,J为11,Q为12,K为13。上面的5张牌就可以变成“1,2,3,4,5”(大小王分别看作2和4),如果牌能组成顺子就输出true，否则就输出false。为了方便起见,你可以认为大小王是0

---
+++ 扑克牌顺子/src/Solution.java
@@ -0,0 +1,24 @@
+import java.util.TreeSet;
+public class Solution {
+    public boolean isContinuous(int [] n) {
+        if (n.length < 5 || n.length > 5) {
+            return false;
+        }
+        int num = 0;
+        TreeSet<Integer> set = new TreeSet<> ();
+        for (int i=0; i<n.length;i++) {
+            if (n[i]==0) {
+                num ++;
+            } else {
+                set.add(n[i]);
+            }
+        }
+        if ((num + set.size()) != 5) {
+            return false;
+        }
+        if ((set.last() - set.first()) < 5) {
+            return true;
+        }
+        return false;
+    }
+}
