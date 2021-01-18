c59c4bd33cc94fb7a4f28a15974bd7fd3ffa872c
e <2540098059@qq.com>
2021-01-18 22:03:22 +0800

    一个机器人在m×n大小的地图的左上角（起点）。 机器人每次向下或向右移动。机器人要到达地图的右下角（终点）。 可以有多少种不同的路径从起点走到终点？

---
+++ 求路径/src/Main.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Main {
+    public int uniquePaths (int m, int n) {
+        int[][] dp = new int[m+1][n+1];
+        for(int i = 1 ; i <= m ; i++){dp[i][1]=1;}//第一列赋值为1
+        for(int j = 1 ; j <= n ; j++){dp[1][j]=1;}//第一行赋值为1
+        for(int i = 2 ; i <= m ; i++)
+        {
+            for(int j = 2 ; j <= n ; j++)
+            {
+                dp[i][j] = dp[i-1][j]+dp[i][j-1];
+            }
+        }
+        return dp[m][n];
+    }
+}
